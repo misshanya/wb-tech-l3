7b34fd2c26183311051fd57472b9dedc0f5fb89a
misshanya <mishanyadev@gmail.com>
2025-11-08 22:57:52 +0300

    feat(comment-tree): frontend: theme toggle

--- comment-tree/frontend/src/App.vue
+++ comment-tree/frontend/src/App.vue
@@ -6,6 +6,17 @@ import CommentItem from './components/CommentItem.vue'
 import Input from './components/ui/input/Input.vue'
 import { buildTree } from './lib/tree'
 import type { CommentNode } from './lib/tree'
+import { useColorMode } from '@vueuse/core'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './components/ui/dropdown-menu'
+import { Icon } from '@iconify/vue'
+import { Button } from '@/components/ui/button'
+
+const mode = useColorMode()
 
 const isLoading = ref(false)
 
@@ -84,6 +95,29 @@ async function handleCommentExpand(parentID: string) {
 </script>
 
 <template>
+  <div class="m-4">
+    <DropdownMenu>
+      <DropdownMenuTrigger as-child>
+        <Button variant="outline">
+          <Icon
+            icon="radix-icons:moon"
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Icon
+            icon="radix-icons:sun"
+            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
+          <span class="sr-only">Сменить тему</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem @click="mode = 'light'"> Светлая </DropdownMenuItem>
+        <DropdownMenuItem @click="mode = 'dark'"> Темная </DropdownMenuItem>
+        <DropdownMenuItem @click="mode = 'auto'"> Системная </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </div>
+
   <div class="max-w-3xl mx-auto mt-8">
     <Input v-model="searchQuery" placeholder="Поиск..." />
     <div class="comments">
